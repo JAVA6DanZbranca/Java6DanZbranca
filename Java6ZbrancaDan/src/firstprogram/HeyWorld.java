329b60890616440be64d646a8f0acae3973ec8fa
Michel D. Zbranca <Michel D. Zbranca@Joat>
2017-09-16 17:08:10 +0300

    This is my first program in Java!

---
+++ Java6ZbrancaDan/src/firstprogram/HeyWorld.java
@@ -0,0 +1,20 @@
+/*
+ * My first program written in Java 
+ */ 
+package firstprogram;
+
+/**
+ *This is my first class
+ * @author Michel D. Zbranca
+ */
+public class HeyWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This is my first program
+        System.out.println("Hello World!");
+    }
+    
+}
